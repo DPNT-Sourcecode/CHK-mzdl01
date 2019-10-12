6549f15f7d08e56867ee4b0193983d598cb71d2d
sbx_user1051 <sbx_user1051@169.254.185.45>
2019-10-12 10:40:24 +0000

    Sat Oct 12 10:40:24 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,19 @@
 
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    offers = {"A": 50, "B": 30 ,"C": 20, "D": 15, "3A": 130, "2B": 45}
+
+    purchases = skus.split(",")
+    res = {}
+    for purchase in purchases:
+        if purchase in offers:
+            res[purchase] = 1
+
+    amount = 0
+
+    print(res)
+
+
+skus = "A,B,C"
+checkout(skus)

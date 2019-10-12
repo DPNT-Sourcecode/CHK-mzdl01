aa25f830747b182fba9078b4f3e53b35e52494ee
sbx_user1051 <sbx_user1051@169.254.185.45>
2019-10-12 09:55:56 +0000

    Sat Oct 12 09:55:56 UTC 2019

--- solutions/SUM.py
+++ solutions/SUM.py
@@ -1,2 +1,8 @@
 def sum_solution(x,y):
-    pass
+    if x < 0 or x > 100:
+        return "x must between 0 - 100"
+    
+    if y < 0 or y > 100:
+        return "y must between 0 - 100"
+
+    return x + y

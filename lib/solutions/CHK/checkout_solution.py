607bb0fec0d1b6e7ad2ac2a13b10b8062e76a29a
sbx_user1051 <sbx_user1051@169.254.185.45>
2019-10-12 11:37:35 +0000

    Sat Oct 12 11:37:35 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,9 +9,6 @@ def checkout(skus):
     B = Item("B", 30, 2, 45)
     C = Item("C", 20)
 
-    # offers = {"3A": 130, "2B": 45}
-    # prices = {"A": 50, "B": 30 ,"C": 20}
-
     available_items = {"A": A, "B": B, "C": C}
     counts = {"A": 0, "B": 0, "C": 0}
     if len(sku) == 0:
@@ -25,13 +22,10 @@ def checkout(skus):
             if counts[purchase] == available_items[purchase].discount_number:
                 amount = amount - (available_items[purchase].amount * 2)
                 amount+= available_items[purchase].discount_price
+                counts[purchase] = 0
             else:
                 amount += available_items[purchase].amount
         else:
             return -1
     
     return amount
-
-
-sku = "AABA"
-print(checkout(sku))

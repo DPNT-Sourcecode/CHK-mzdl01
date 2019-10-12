910f1b56b67958f9d9682e660f8237a85b7079d0
sbx_user1051 <sbx_user1051@169.254.185.45>
2019-10-12 11:22:35 +0000

    Sat Oct 12 11:22:35 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,36 +2,39 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 from item import Item
-def checkout(skus):
-    A = Item("A", 50, 3, 130)
-    B = Item("A", 30, 2, 45)
-    C = Item("A", 20)
-    offers = {"3A": 130, "2B": 45}
-    prices = {"A": 50, "B": 30 ,"C": 20}
-    if len(sku) == 0:
-        return 0
+# def checkout(skus):
+    # A = Item("A", 50, 3, 130)
+    # B = Item("B", 30, 2, 45)
+    # C = Item("C", 20)
+    # offers = {"3A": 130, "2B": 45}
+    # prices = {"A": 50, "B": 30 ,"C": 20}
 
-    res = {}
-    for purchase in skus:
-        if purchase in offers:
-            if purchase in res:
-                res[purchase] += 1
-            else:
-                res[purchase] = 1
-        else:
-            return -1
+    # if len(sku) == 0:
+    #     return 0
+    # amount = 0
+    # counts = {}
+    # for purchase in skus:
+        
+    #     if purchase in offers:
+    #         if purchase in res:
+    #             res[purchase] += 1
+    #         else:
+    #             res[purchase] = 1
+    #     else:
+    #         return -1
 
-    amount = 0
+    # amount = 0
 
-    for key, val in res.items():
-        off = str(val) + str(key)
-        if off in offers:
-            amount += offers[off]
-        else:
-            amount+= offers[key] * val
+    # for key, val in res.items():
+    #     off = str(val) + str(key)
+    #     if off in offers:
+    #         amount += offers[off]
+    #     else:
+    #         amount+= offers[key] * val
 
-    return amount
+    # return amount
 
 
-sku = "AAAA"
-print(checkout(sku))
+sku = "ABAA"
+print(sorted(sku))
+# print(checkout(sku))

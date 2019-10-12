3256acd132f20e464650b46501db2afc1050fd44
sbx_user1051 <sbx_user1051@169.254.185.45>
2019-10-12 11:44:26 +0000

    Sat Oct 12 11:44:26 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,9 +7,10 @@ def checkout(skus):
     A = {"price": 50, "discount_number": 3, "discount_price": 130} 
     B = {"price": 30, "discount_number": 2, "discount_price": 145}
     C = {"price": 20, "discount_number": 0, "discount_price": 0}
+    D = {"price": 15, "discount_number": 0, "discount_price": 0}
 
-    available_items = {"A": A, "B": B, "C": C}
-    counts = {"A": 0, "B": 0, "C": 0}
+    available_items = {"A": A, "B": B, "C": C, "D": D}
+    counts = {"A": 0, "B": 0, "C": 0, "D": 0}
     if len(skus) == 0:
         return 0
 
@@ -28,4 +29,4 @@ def checkout(skus):
     
     return amount
 
-print(checkout("AAAA"))
+print(checkout("ABCD"))

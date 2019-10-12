896b3067cd6f4c68face5b1f5bb573f92482b952
sbx_user1051 <sbx_user1051@169.254.185.45>
2019-10-12 11:40:35 +0000

    Sat Oct 12 11:40:35 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,17 +1,16 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-from item import Item
 
 def checkout(skus):
     purchases = sorted(skus)
-    A = Item("A", 50, 3, 130)
-    B = Item("B", 30, 2, 45)
-    C = Item("C", 20)
+    A = {"price": 50, "discount_number": 3, "discount_price": 130} 
+    B = {"price": 30, "discount_number": 2, "discount_price": 145}
+    C = {"price": 20, "discount_number": 0, "discount_price": 0}
 
     available_items = {"A": A, "B": B, "C": C}
     counts = {"A": 0, "B": 0, "C": 0}
-    if len(sku) == 0:
+    if len(skus) == 0:
         return 0
 
     amount = 0
@@ -19,13 +18,15 @@ def checkout(skus):
         if purchase in available_items:
             print(purchase)
             counts[purchase] += 1
-            if counts[purchase] == available_items[purchase].discount_number:
-                amount = amount - (available_items[purchase].amount * 2)
-                amount+= available_items[purchase].discount_price
+            if counts[purchase] == available_items[purchase][discount_number]:
+                amount = amount - (available_items[purchase][amount] * 2)
+                amount+= available_items[purchase][discount_price]
                 counts[purchase] = 0
             else:
-                amount += available_items[purchase].amount
+                amount += available_items[purchase][amount]
         else:
             return -1
     
     return amount
+
+print(checkout("AAAA"))
